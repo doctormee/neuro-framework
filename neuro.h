3d11247d5b5fc6e74d77beee91fc5296406bbf86
Вадим Сливинский <vadim@iMac-Vadim.local>
2016-09-24 21:59:10 +0300

    another small test change

--- neuro.h
+++ neuro.h
@@ -17,42 +17,43 @@ values and methods
 class Node{
     double y; //output value
     double F( double x ); //activation
-    static double S( E_list *inp ) //computing function, is static for all nodes and (in our scenario) is a weighted sum
-    {
-        double val = 0;
-        if ( *inp != NULL ){
-            while ( *inp.next != NULL ){
-                val += *inp.edge.from.get_y() * *inp.edge.weight; 
-                *inp = *inp.next;
-            }
-        }
-        return val;
-    }
-    E_list input_list, output_list; //these are two E_lists storing all edges going in and out of the node
+    static double S( E_list *inp ); //computing function, is static for all nodes and (in our scenario) is a weighted sum
+    E_list *input_list, *output_list; //these are two E_lists storing all edges going in and out of the node
     // enum n_type{ input, hidden, output, bias }
 public:
-    static void compute(){
-        y = F( S( input_list ) );
-    }
-    static void sety( double x )
-    {
-        y = x;
-    }
-    static double gety(){
-        return y;
-    }
+    void compute() const { y = F( S( input_list ) ); } //calculates and sets a proper output value 
+    void sety( double x ) const { y = x; } //y setter
+    double gety() const { return y; } //y getter
 };
 /* this is a Layer structure, used by Net */
 struct Layer{
     Node *node;
     Layer *next;
 };
+/* this is the same as E_list, except this one points on layers */
+struct L_list{
+    Layer *layer;
+    L_list *next;
+};
 /* this is a main Net class, which is a neuro-net 
 it should provide proper interface
 */
 class Net{
     layer input_layer, output_layer;
-    struct hidden_layers{
-        
-    };
+    L_list *hidden_layers;
+public:
+    
+};
+/* this section is solely for defining Node static methods */
+double Node::S( E_list *inp )
+{
+    double val = 0;
+    if ( *inp != NULL ){
+        while ( *inp.next != NULL ){
+            val += *inp.edge.from.get_y() * *inp.edge.weight; 
+            *inp = *inp.next;
+        }
+    }
+    return val;
 }
+    
