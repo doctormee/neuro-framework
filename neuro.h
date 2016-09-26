6e36425f026aedc7813ae91730a75c14ab9add02
Doctor_mee <wandrsl@gmail.com>
2016-09-26 18:49:50 +0300

    net constructor

--- neuro.h
+++ neuro.h
@@ -1,4 +1,6 @@
-
+#include <new>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 double sgn(double val) {
     return double ( ( 0 < val ) - ( val < 0 ) );
 }
@@ -8,7 +10,7 @@ It stores index and destination Node pointers and a weight of an edge
 struct Edge{
     Node *from, *to;
     double weight;
-    Edge( Node *f, Node *t, double w );
+    Edge( Node, Node, double );
 };
 
 /* this is a list*/
@@ -17,8 +19,8 @@ struct List{
     T *elem;
     List *next;
     List();
-    List( T *init );
-    void link( T *piece ); // links *piece at the end of a list 
+    List( T );
+    void append( T ); // appends *piece at the end of a list 
 };
 /* this is a main class, implementing a node structure with all necessary 
 values and methods
@@ -26,12 +28,12 @@ values and methods
 class Node{
     double activation_value; //output value
     double ( *activation_function )( double ); //pointer to the activation function
-    static double computing_function( List <Edge> inp ); //computing function, is static for all nodes and (in our scenario) is a weighted sum
+    static double computing_function( List <Edge>  ); //computing function, is static for all nodes and (in our scenario) is a weighted sum
     // enum n_type{ input, hidden, output, bias }
 public:
     List <Edge> input_list, output_list; //these are two Edge lists storing all edges going in and out of the node
     Node(){ activation_value = 0; activation_function = sgn;} //simple constructor, used for making a Bias
-    Node( double val, double ( *func )( double ) );
+    Node( double ( *func )( double ) );
     void compute() const { y = activation_function( computing_function( input_list ) ); } //calculates and sets a proper output value 
     void set_activation_value( double x ) const { activation_value = x; } // setter
     double get_activation_value() const { return activation_value; } // getter
@@ -44,9 +46,13 @@ it should provide proper interface
 class Net{
     List <Node> input_layer, output_layer;
     List <List <Node> > hidden_layers;
+    List <Edge> edges;
 public:
     Net( int hidden_layer_count, int input_node_count, int hidden_node_count, int output_node_count, 
-    double ( *hidden_activation_function)( double ), double ( *output_activation_function)( double ) ); //node constructior
+    double ( *hidden_activation_function)( double ), double ( *output_activation_function)( double ) ); //net constructior
+    ~Net();
+    List <double> activate( List <double> input ); //attnt: input is set by val, not by ref
+    void back_propagate( List <double> output, List <double> ideal );
 };
 //
 //
@@ -58,8 +64,8 @@ Edge::Edge( Node *f, Node *t, double w ){
     to = t;
     from = f;
     weight = w;
-    *f.output_list.link( this );
-    *t.input_list.link( this );
+    *f.output_list.append( this );
+    *t.input_list.append( this );
 }
 /* this section is solely for defining List methods */
 template <class T>
@@ -73,7 +79,7 @@ List<T>::List( T *init ){
     elem = init; 
 }
 template <class T>
-void List<T>::link( T *piece ) {
+void List<T>::append( T *piece ) {
     List** temp;
     *temp = next;
     while ( *temp != nullptr ){
@@ -91,8 +97,56 @@ double Node::computing_function( List <Edge> inp ){
     }
     return val;
 }
-Node::Node( double val, double ( *func )( double ) ){
-    activation_value = val;
+Node::Node( double ( *func )( double ) ){
+    activation_value = 0;
     activation_function = func;
+}
+/* this section is solely for defining Net methods */
+Net::Net( int hidden_layer_count, int input_node_count, int hidden_node_count, int output_node_count, 
+double ( *hidden_activation_function)( double ), double ( *output_activation_function)( double ) ){
+    srand( time( nullptr ) );
+    //initializing input_layer
+    while ( input_node_count-- ){
+        input_layer.append( new Node );
+    }
+    //initializing all hidden layers
+    int nodes_left;
+    List <Node> *hidden_layer;
+    List <Node> *link_layer = &input_layer; 
+    Node *new_node, *link_node;
+    List <List <Node> > *prev_hidden_layer = nullptr; 
+    while ( hidden_layer_count-- ){
+        hidden_layer = new List;
+        nodes_left = hidden_node_count;
+        while ( nodes_left-- ){
+            new_node = new Node( hidden_activation_function );
+            *hidden_layer.append( new_node );
+            link_node = *link_layer.elem;
+            while ( link_node != nullptr ){
+                edges.append( new Edge( link_node, new_node, rand() % 200 - 100 ) ); //initializes a random-weighted ( -100 -- 100 ) edge between link_node and new_node
+                link_node = *link_layer.next;
+            }
+        }
+        //and finally append our new initialized and linked layer to the hidden_layers
+        hidden_layers.append( *hidden_layer );
+        //finding the next link_layer might be tricky
+        if ( prev_hidden_layer == nullptr ){
+            prev_hidden_layer = &hidden_layers; 
+        }
+        else{
+            prev_hidden_layer = *prev_hidden_layer.next; 
+        }
+        //yet we set it here
+        link_layer = *prev_hidden_layer.elem; 
+    }
+    while ( output_node_count-- ){
+        new_node = new Node( output_activation_function );
+        output_layer.append( new_node );
+        link_node = *link_layer.elem;
+        while ( link_node != nullptr ){
+            edges.append( new Edge( link_node, new_node, rand() % 200 - 100 ) ); //initializes a random-weighted ( -100 -- 100 ) edge between link_node and new_node
+            link_node = *link_layer.next;
+        }
+    }
 }
     
